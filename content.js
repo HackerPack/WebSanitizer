8c02e26a040fb386ac0606c259e0f521de57c30b
ssharm21 <ssharm21@ncsu.edu>
2016-10-29 16:08:12 -0400

    collecting images in a list

---
+++ content.js
@@ -0,0 +1,7 @@
+// content.js
+alert("Hello from your Chrome extension!")
+//var firstHref = $("a[href^='http']").eq(0).attr("img");
+var srcList = $('img').map(function() {
+    return this.src;
+}).get();
+console.log(srcList);
