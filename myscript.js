b9d0d53778288af0ec3343c93032d5aa4f8063e5
Shashwath Kumar <shashwath.basics@gmail.com>
2016-10-29 22:13:57 -0400

    trying to integrate

--- myscript.js
+++ myscript.js
@@ -4,6 +4,26 @@
 //     rawFile.onreadystatechange = function ()
 //     {
 //         if(rawFile.readyState === 4)
+
+chrome.webRequest.onBeforeRequest.addListener(
+       function(details) {
+            //alert(details.url);
+            var url = details.url;
+            if(details.url.indexOf("images")!= -1){
+            	//console.log('change url');
+                // url = "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif";
+                url = sendToClarifi(url);
+            }
+			return {
+   				redirectUrl: url
+ 			};
+ 		},
+       {urls: ["<all_urls>"], types:["image"]},
+       ["blocking"]);
+
+
+
+
 //         {
 //             var text = rawFile.responseText;
 //         }
@@ -22,35 +42,65 @@ var badDict = {}
 for (var i=0; i<badWords.length; i++) {
 	badDict[badWords[i]] = true;
 }
+/*
+// var srcList = $('img').get();
 
-var srcList = $('img').get();
+// console.log(srcList);
 
-console.log(srcList);
+// // setTimeout(function(){
+// //     console.log("THIS IS");
+// // }, 5000);
 
-// setTimeout(function(){
-//     console.log("THIS IS");
-// }, 5000);
-
-for (var i=0;i<srcList.length;i++) {
-	console.log(srcList[i]);
-	sendToClarifi(srcList[i].src);
-}
+// for (var i=0;i<srcList.length;i++) {
+// 	console.log(srcList[i]);
+// 	sendToClarifi(srcList[i].src);
+// }
 //sendToClarifi('https://drscdn.500px.org/photo/155841899/q%3D80_m%3D1500/65a62d508837176ed2b6eae08943980d');
+console.log('myscripts');
 
+chrome.webRequest.onBeforeRequest.addListener(
+       function(details) {
+            //alert(details.url);
+            var url = details.url;
+            console.log(url);
+            if(details.url.indexOf("images")!= -1){
+
+            	url = sendToClarifi(url);
+                // url = "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif (492KB)";
+            }
+		return {
+   			redirectUrl: url
+ 			};
+ 		},
+       {urls: ["<all_urls>"], types:["image"]},
+       ["blocking"]);
+
+*/
 function sendToClarifi(link){
+  // alert('sendToClarifi');
+  alert('FIRST: '+link);
   $.ajax({
   type: "GET",
   //url: "https://api.clarifai.com/v1/tag/?url=" + link+'&access_token=3MdWoUytY3a3QuXxdBl7oRkwqyql7A',
   url: "https://api.clarifai.com/v1/tag/?url=" + link+'&access_token=3MdWoUytY3a3QuXxdBl7oRkwqyql7A',
   success: function(response){
-    if (isProfane(response.results[0].result.tag.classes))
-    	console.log("PROFANE");
-    else
-    	console.log("NOT PROFANE")
+    if (isProfane(response.results[0].result.tag.classes)){
+    	link = "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif";
+    	// console.log("PROFANE");
+    	alert("PROFANE");
+    	alert('SECOND: '+link);
+    	}
+    else{
+    	// console.log("NOT PROFANE");
+    	alert("NOT PROFANE");
+    	link = "http://oi34.tinypic.com/2prcnly.jpg";
+    	alert('THIRD: '+link);
+    }
   },
   dataType: "json"
 });
-
+  alert('FOURTH: '+link);
+  return link;
 }
 
 function isProfane(tagList) {
@@ -64,4 +114,3 @@ function isProfane(tagList) {
 	}
 	return false;
 }
-
